020929dc67c8d90737ff1494cd0b6f899aaaaed3
rahul beerh <rahulbeerh82@gmail.com>
2024-07-12 13:02:35 +0530

    gg

--- src/Data/Api.js
+++ src/Data/Api.js
@@ -1,5 +1,6 @@
-// const pre=`https://reports.visiontrek.in/`;
-const pre=`/`;
+const pre=`https://reports.visiontrek.in/`;
+// const pre="http://192.168.1.11:4041/"
+// const pre=`/`;
 
 const loginApi = `${pre}open/login`;
 export { loginApi };
